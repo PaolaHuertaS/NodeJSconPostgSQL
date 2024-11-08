d9842addb6977cafd487c9faede7e680ac6d8d94
nicol <nicolehs2015@hotmail.com>
2024-11-08 16:03:25 -0600

    Segundo commit, meter postgrsql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false, 
+      ssl: {
+        rejectUnauthorized: false 
+      }
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
