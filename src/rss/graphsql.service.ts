33ea2950ff76e98858c7bd5083b0ce5f4ad60c1b
nicol <nicolehs2015@hotmail.com>
2025-02-17 17:43:48 -0600

    implemente libreria graphql, así como metodo base explicando lo que leí, junto a la URL de la pagina donde encontre

---
+++ src/rss/graphsql.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class GraphQLService {
+  //url que se usará en todo el código,asi se podra reutilizar
+  private readonly API_URL = 'https://graphql.anilist.co';
+
+  //método base
+
+}
